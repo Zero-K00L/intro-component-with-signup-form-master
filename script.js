7c5b099aa3ba8076d354290441a85a3b7b24ab67
John Grudzinski <johngrudzinski@yahoo.com>
2023-09-28 16:32:24 -0400

    Input Error Fixes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const errorBorder = document.querySelector('.input');
+const inputField = document.querySelector('.input');
 const errorIcon = document.querySelector('.error-icon');
 const errorMessage = document.querySelector('.error-message');
 
@@ -14,22 +14,19 @@ const lastNameInput = document.querySelector('.last-name');
 const emailInput = document.querySelector('.email');
 const passwordInput = document.querySelector('.password');
 
+
 // Each error message
 const errorMessageFirstName = document.querySelector('.error-message-1');
 const errorMessageLastName = document.querySelector('.error-message-2');
 const errorMessageEmail = document.querySelector('.error-message-3');
 const errorMessagePassword = document.querySelector('.error-message-4');
 
+
 const form = document.querySelector('.form');
 
 const submitBtn = document.querySelector('.form-btn');
 
 submitBtn.addEventListener('click', function(event) {
-    // reset errors
-    resetErrorStyles(); 
-
-    // Validates the input fields
-
     if(firstNameInput.value.trim() === '') {
         displayError(errorIconFirstName, errorMessageFirstName, firstNameInput);
         event.preventDefault();
@@ -46,25 +43,52 @@ submitBtn.addEventListener('click', function(event) {
       }
 
 
+    const passwordValue = passwordInput.value.trim();
+    const passwordError = document.querySelector('.password-length-error');  
+
     if(passwordInput.value.trim() === '') {
         displayError(errorIconPassword, errorMessagePassword, passwordInput);
         event.preventDefault();
     }
+
+
+    else if (passwordValue.length < 8) {
+        displayError(errorIconPassword, passwordError, passwordInput)
+        event.preventDefault();
+        console.log(passwordValue.length);
+    }
 });
 
+
+// Reset errors when the user starts typing
+// Add an event listener to each input field to reset errors when the user starts typing
+firstNameInput.addEventListener('input', function () {
+    // Reset error styles for the "First Name" field
+    resetErrorStyles(errorIconFirstName, errorMessageFirstName, firstNameInput);
+});
+
+lastNameInput.addEventListener('input', function () {
+    // Reset error styles for the "Last Name" field
+    resetErrorStyles(errorIconLastName, errorMessageLastName, lastNameInput);
+});
+
+emailInput.addEventListener('input', function () {
+    // Reset error styles for the "Email" field
+    resetErrorStyles(errorIconEmail, errorMessageEmail, emailInput);
+});
+
+passwordInput.addEventListener('input', function () {
+    // Reset error styles for the "Password" field
+    resetErrorStyles(errorIconPassword, errorMessagePassword, passwordInput);
+});
+
+
+
 // Function to reset error styles
-function resetErrorStyles() {
-    // Reset the display styles for all error icons and messages
-    const allErrorIcons = document.querySelectorAll('.error-icon');
-    const allErrorMessages = document.querySelectorAll('.error-message');
-
-    allErrorIcons.forEach(icon => {
-        icon.style.display = 'none';
-    });
-
-    allErrorMessages.forEach(message => {
-        message.style.display = 'none';
-    });
+function resetErrorStyles(errorIcon, errorMessage, inputField) {
+    errorIcon.style.display = 'none';
+    errorMessage.style.display = 'none';
+    inputField.style.border = ''; // Reset the border to its default state
 }
 
 // Function to display error message and icon
@@ -76,8 +100,6 @@ function displayError(errorIcon, errorMessage, inputField) {
 
 // Function to validate email format
 function isValidEmail(email) {
-    function isValidEmail(email) {
-        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-        return emailPattern.test(email);
-    }
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailPattern.test(email);
 }
